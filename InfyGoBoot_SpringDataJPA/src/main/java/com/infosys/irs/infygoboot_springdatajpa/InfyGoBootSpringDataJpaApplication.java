d3463fa556465980e177508813b0243ccd99107b
gauravgaikwad242 <gaikwadg576@gmail.com>
2022-06-14 20:25:07 +0530

    demo initialization

---
+++ InfyGoBoot_SpringDataJPA/src/main/java/com/infosys/irs/infygoboot_springdatajpa/InfyGoBootSpringDataJpaApplication.java
@@ -0,0 +1,14 @@
+package com.infosys.irs.infygoboot_springdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyGoBootSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyGoBootSpringDataJpaApplication.class, args);
+	}
+
+}
+
